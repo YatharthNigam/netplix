68985f90095bac673a0a8d1131262adc3958247d
Yatharth Nigam <yatharthnigam@gmail.com>
2022-12-21 16:57:56 +0530

    implemented the movies view section.

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -3,14 +3,39 @@ import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material'
 import { useSelector } from 'react-redux';
 
 import { useGetMoviesQuery } from '../../services/TMDB';
+import { MovieList } from '..';
 
 function Movies() {
-  const { data } = useGetMoviesQuery();
+  const { data, error, isFetching } = useGetMoviesQuery();
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies that match that name.
+          <br />
+          Please seach for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return 'An error has occurred.';
 
   console.log(data);
 
   return (
-    <div>Movies</div>
+    <div>
+      <MovieList movies={data} />
+    </div>
   );
 }
 
